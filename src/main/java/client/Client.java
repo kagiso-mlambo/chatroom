a86b4a55c89fb70ec6173167aaf30febde847e43
Kagiso Mlambo <missk.charmaine@gmail.com>
2026-06-03 21:51:03 +0200

    fix: Removed the static from all the Client attributes and created instance methods for the data.

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -5,32 +5,58 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Client {
-    private static final Scanner sc = new Scanner(System.in);
-    private static String username;
-    private static Socket clientSocket;
-    private static PrintWriter printWriter;
-    private static BufferedReader bufferedReader;
+    private String username;
+    private Socket clientSocket;
+    private PrintWriter writer;
+    private BufferedReader reader;
+
+    public Client() throws IOException{
+        writer = new PrintWriter(clientSocket.getOutputStream(), true);
+        reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    }
+
+    public void setClientSocket(Socket clientSocket){ this.clientSocket = clientSocket; }
+
+    public void setUsername(String username){ this.username = username; }
+
+    public String username(){ return username; }
+
+    public PrintWriter writer(){ return writer; }
+
+    public BufferedReader reader(){ return reader; }
 
 
-    static void main(String[] args) throws IOException{
-        System.out.println("Please enter your username: ");
-        username = sc.nextLine();
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+    public static void main(String[] args) {
+        try {
+            Client client = new Client();
+            Scanner scanner = new Scanner(System.in);
+            String request;
 
-        clientSocket = new Socket("localhost",8081);
-        printWriter = new PrintWriter(clientSocket.getOutputStream(), true);
-        bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        new Thread(new ListenForMessages(bufferedReader)).start();
+            System.out.println("Please enter your username: ");
+            request = scanner.nextLine();
+            client.setUsername(request);
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
 
-        printWriter.println(username);
+            Socket clientSocket = new Socket("localhost", 8081);
+            client.setClientSocket(clientSocket);
 
-        String request;
-        while (true){
-            request = sc.nextLine();
-            printWriter.println(request);
-            System.out.print("\033[1A\033[2K");
-            if (request.equalsIgnoreCase("quit")) { System.exit(0);}
+            PrintWriter writer = client.writer();
+            BufferedReader reader = client.reader();
+            new Thread(new ListenForMessages(reader)).start();
+
+            writer.println(request);
+
+            while (request != null) {
+                request = scanner.nextLine();
+                writer.println(request);
+                System.out.print("\033[1A\033[2K");
+                if (request.equalsIgnoreCase("quit")) {
+                    System.exit(0);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("The Server is currently unavailable");
         }
     }
 
