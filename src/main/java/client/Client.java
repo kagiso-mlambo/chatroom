4a150fe1c5ed6170352760c99c1ed55ec7d46bd6
Kagiso Mlambo <missk.charmaine@gmail.com>
2026-06-03 21:56:18 +0200

    fix: Removed the attributes completely as they are truly not needed and can be initialised locally in main.

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -5,44 +5,20 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Client {
-    private String username;
-    private Socket clientSocket;
-    private PrintWriter writer;
-    private BufferedReader reader;
-
-    public Client() throws IOException{
-        writer = new PrintWriter(clientSocket.getOutputStream(), true);
-        reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-    }
-
-    public void setClientSocket(Socket clientSocket){ this.clientSocket = clientSocket; }
-
-    public void setUsername(String username){ this.username = username; }
-
-    public String username(){ return username; }
-
-    public PrintWriter writer(){ return writer; }
-
-    public BufferedReader reader(){ return reader; }
-
-
     public static void main(String[] args) {
         try {
-            Client client = new Client();
             Scanner scanner = new Scanner(System.in);
             String request;
 
             System.out.println("Please enter your username: ");
             request = scanner.nextLine();
-            client.setUsername(request);
             System.out.print("\033[H\033[2J");
             System.out.flush();
 
             Socket clientSocket = new Socket("localhost", 8081);
-            client.setClientSocket(clientSocket);
 
-            PrintWriter writer = client.writer();
-            BufferedReader reader = client.reader();
+            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             new Thread(new ListenForMessages(reader)).start();
 
             writer.println(request);
